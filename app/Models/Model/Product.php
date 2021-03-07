e589f49dbf494442b6b5bc4eb2fc9224b714f9fa
Jdev1 <kooreal@gmail.com>
2021-03-06 19:15:50 -0500

    Worked on review and product relationship

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -4,8 +4,17 @@ namespace App\Models\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+		'name','detail','stock','price','discount'
+	];
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class); 
+    }
+    
 }
--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -4,8 +4,17 @@ namespace App\Models\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = [
+		'star','customer','review'
+	];
+    public function product()
+    {
+    	return $this->belongsTo(Product::class); 
+    }
+
 }
